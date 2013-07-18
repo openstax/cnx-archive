d7b4af0d8ab331275229cbde999bd545deb9eb16
Michael Mulich <michael.mulich@gmail.com>
2013-07-17 22:00:39 -0400

    Database table population files.

---
+++ cnxarchive/sql/constant-tags.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO tags VALUES (0, 'Test/Draft', 'internal');
+INSERT INTO tags VALUES (1, 'Arts', 'ISKME subject');
+INSERT INTO tags VALUES (2, 'Business', 'ISKME subject');
+INSERT INTO tags VALUES (3, 'Humanities', 'ISKME subject');
+INSERT INTO tags VALUES (4, 'Mathematics and Statistics', 'ISKME subject');
+INSERT INTO tags VALUES (5, 'Science and Technology', 'ISKME subject');
+INSERT INTO tags VALUES (6, 'Social Sciences', 'ISKME subject');
