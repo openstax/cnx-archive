52aa86d47e97a21412117119a113f36308097843
Michael Mulich <michael.mulich@gmail.com>
2013-07-17 21:09:53 -0400

    It's a package.

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+install_requires = (
+    )
+description = "An archive for Connexions documents."
+
+
+setup(
+    name='cnx-archive',
+    version='0.1',
+    author='Connexions team',
+    author_email='info@cnx.org',
+    url="https://github.com/connexions/cnx-archive",
+    license='LGPL, See aslo LICENSE.txt',
+    description=description,
+    packages=find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
+    entry_points="""\
+    """,
+    test_suite='cnxarchive.tests'
+    )
