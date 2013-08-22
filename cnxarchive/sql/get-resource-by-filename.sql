ea31035880cbf441f2abbef682d95d680ee2a81e
Michael Mulich <michael.mulich@gmail.com>
2013-08-22 14:38:38 -0400

    Join on the module to allow the uuid matching condition.

--- cnxarchive/sql/get-resource-by-filename.sql
+++ cnxarchive/sql/get-resource-by-filename.sql
@@ -1,5 +1,6 @@
 -- arguments: id:string; filename:string
-SELECT file
+SELECT f.file
 FROM module_files as mf
-LEFT JOIN files f on mf.fileid = f.fileid
-WHERE mf.uuid = %(id)s AND mf.filename = %(filename)s;
+  LEFT JOIN files f on mf.fileid = f.fileid
+  LEFT JOIN modules m on mf.module_ident = m.module_ident
+WHERE m.uuid = %(id)s AND mf.filename = %(filename)s;
