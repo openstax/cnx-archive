1cb91ed240037cde02964ca19c4962e9d82b811f
Michael Mulich <michael.mulich@gmail.com>
2013-08-22 14:37:41 -0400

    Fixes the abstract join. There is a problem with this query when an abstract doesn't exist. And the database doesn't seem to enforce a contraint on this relationship.

--- cnxarchive/sql/get-module-metadata.sql
+++ cnxarchive/sql/get-module-metadata.sql
@@ -2,18 +2,23 @@
 SELECT row_to_json(combined_rows) as module
 FROM (SELECT
   m.uuid AS id, m.version, m.name, m.created as created, m.revised as revised,
-  abstract, m.stateid, m.doctype,l.url AS license, m.module_ident AS ident, m.submitter, m.submitlog, m.portal_type as type,
+  m.stateid, m.doctype,l.url AS license, m.module_ident AS ident,
+  m.submitter, m.submitlog, m.portal_type as type,
+  a.abstract,
   p.moduleid AS parent_id, p.version AS parent_version,
   m.authors as authors, m.licensors as licensors, m.maintainers as maintainers, COALESCE(m.parentauthors,ARRAY(select ''::text where false)) as "parentAuthors", m.language as language, (select '{'||list(''''||roleparam||''':['''||array_to_string(personids,''',''')||''']')||'}' from roles natural join moduleoptionalroles where module_ident=m.module_ident group by module_ident) as _roles, list(tag) as _subject
 FROM modules m
-NATURAL JOIN abstracts
-LEFT JOIN modules p on m.parent = p.module_ident
-LEFT JOIN moduletags mt on m.module_ident = mt.module_ident NATURAL LEFT JOIN tags,
-licenses l
+  LEFT JOIN abstracts a on m.abstractid = a.abstractid
+  LEFT JOIN modules p on m.parent = p.module_ident
+  LEFT JOIN moduletags mt on m.module_ident = mt.module_ident
+  NATURAL LEFT JOIN tags, licenses l
 WHERE
-m.licenseid = l.licenseid AND
-m.uuid = %(id)s AND
-m.version = %(version)s
+  m.licenseid = l.licenseid AND
+  m.uuid = %(id)s AND
+  m.version = %(version)s
 GROUP BY
-m.moduleid, m.portal_type, m.version, m.name, m.created, m.revised, abstract, m.stateid, m.doctype, l.url, m.module_ident, m.submitter, m.submitlog, p.moduleid, p.version, m.authors, m.licensors, m.maintainers, m.parentauthors, m.language
+  m.moduleid, m.portal_type, m.version, m.name, m.created, m.revised,
+  a.abstract, m.stateid, m.doctype, l.url, m.module_ident, m.submitter,
+  m.submitlog, p.moduleid, p.version, m.authors, m.licensors, m.maintainers,
+  m.parentauthors, m.language
 ) combined_rows ;
