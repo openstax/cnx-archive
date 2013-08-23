3a73123368d42135c5360f5032252dbaf88ababc
Ross Reedstrom <reedstrm@gmail.com>
2013-08-23 16:44:08 -0500

    add copyright statement and license info to sql files

--- cnxarchive/sql/get-resource-by-filename.sql
+++ cnxarchive/sql/get-resource-by-filename.sql
@@ -1,3 +1,10 @@
+-- ###
+-- Copyright (c) 2013, Rice University
+-- This software is subject to the provisions of the GNU Affero General
+-- Public License version 3 (AGPLv3).
+-- See LICENCE.txt for details.
+-- ###
+
 -- arguments: id:string; filename:string
 SELECT f.file
 FROM module_files as mf
--- cnxarchive/sql/shred_collxml.sql
+++ cnxarchive/sql/shred_collxml.sql
@@ -1,3 +1,10 @@
+-- ###
+-- Copyright (c) 2013, Rice University
+-- This software is subject to the provisions of the GNU Affero General
+-- Public License version 3 (AGPLv3).
+-- See LICENCE.txt for details.
+-- ###
+
 create  or replace function shred_collxml (doc text) returns void
 as $$
 
