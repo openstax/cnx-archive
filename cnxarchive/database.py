c6ebf344f054170615050cb3da78e78f3e84277e
Michael Mulich <michael.mulich@gmail.com>
2013-08-27 08:31:47 -0400

    Include the shred_collxml definition in the initdb function.

--- cnxarchive/database.py
+++ cnxarchive/database.py
@@ -15,9 +15,10 @@ CONNECTION_SETTINGS_KEY = 'db-connection-string'
 here = os.path.abspath(os.path.dirname(__file__))
 SQL_DIRECTORY = os.path.join(here, 'sql')
 DB_SCHEMA_DIRECTORY = os.path.join(SQL_DIRECTORY, 'schema')
-DB_SCHEMA_FILES = (
+DB_SCHEMA_FILE_PATHS = (
     os.path.join(DB_SCHEMA_DIRECTORY, 'main.sql'),
     os.path.join(DB_SCHEMA_DIRECTORY, 'trees.sql'),
+    os.path.join(DB_SCHEMA_DIRECTORY, 'shred_collxml.sql')
     )
 
 
@@ -39,7 +40,7 @@ def initdb(settings):
     """
     with psycopg2.connect(settings[CONNECTION_SETTINGS_KEY]) as db_connection:
         with db_connection.cursor() as cursor:
-            for schema_filepath in DB_SCHEMA_FILES:
+            for schema_filepath in DB_SCHEMA_FILE_PATHS:
                 with open(schema_filepath, 'r') as f:
                     cursor.execute(f.read())
             sql_constants = [os.path.join(DB_SCHEMA_DIRECTORY, filename)
