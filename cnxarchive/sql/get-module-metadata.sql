3f22994c3f03339de172ea4a9167bf0701a5d85e
Michael Mulich <michael.mulich@gmail.com>
2013-08-27 23:53:15 -0400

    Clearer spacing and grouping...

--- cnxarchive/sql/get-module-metadata.sql
+++ cnxarchive/sql/get-module-metadata.sql
@@ -8,12 +8,19 @@
 -- arguments: id:string; version:string
 SELECT row_to_json(combined_rows) as module
 FROM (SELECT
-  m.uuid AS id, m.version, m.name, m.created as created, m.revised as revised,
-  m.stateid, m.doctype,l.url AS license, m.module_ident AS ident,
+  m.uuid AS id, m.version, m.name,
+  m.created as created, m.revised as revised,
+  m.stateid, m.doctype,
+  l.url AS license, m.module_ident AS ident,
   m.submitter, m.submitlog, m.portal_type as type,
   a.abstract,
   p.moduleid AS parent_id, p.version AS parent_version,
-  m.authors as authors, m.licensors as licensors, m.maintainers as maintainers, COALESCE(m.parentauthors,ARRAY(select ''::text where false)) as "parentAuthors", m.language as language, (select '{'||list(''''||roleparam||''':['''||array_to_string(personids,''',''')||''']')||'}' from roles natural join moduleoptionalroles where module_ident=m.module_ident group by module_ident) as _roles, list(tag) as _subject
+  m.authors as authors, m.licensors as licensors, m.maintainers as maintainers,
+  COALESCE(m.parentauthors,
+           ARRAY(select ''::text where false)) as "parentAuthors",
+  m.language as language,
+  (select '{'||list(''''||roleparam||''':['''||array_to_string(personids,''',''')||''']')||'}' from roles natural join moduleoptionalroles where module_ident=m.module_ident group by module_ident) as _roles,
+  list(tag) as _subject
 FROM modules m
   LEFT JOIN abstracts a on m.abstractid = a.abstractid
   LEFT JOIN modules p on m.parent = p.module_ident
