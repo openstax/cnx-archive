5f159412306469450977128584fe9cb7e46ec484
Michael Mulich <michael.mulich@gmail.com>
2013-09-09 13:57:32 -0400

    Adds the fixed set of roles at database initialization.

---
+++ cnxarchive/sql/schema/constant-roles.sql
@@ -0,0 +1,11 @@
+INSERT INTO roles (roleid, roleparam, rolename, roledisplayname, roleattribution, rolecomment) 
+VALUES (1, 'authors', 'Author', 'Authors', 'Written by:', 'Intellectual author of the work.');
+INSERT INTO roles (roleid, roleparam, rolename, roledisplayname, roleattribution, rolecomment) 
+VALUES (2, 'licensors', 'Licensor', 'Copyright Holders', 'Copyright by:', 'Legal rights holder of the work.');
+INSERT INTO roles (roleid, roleparam, rolename, roledisplayname, roleattribution, rolecomment) 
+VALUES (3, 'maintainers', 'Maintainer', 'Maintainers', 'Maintained by:', 'Has technical permission to republish the work.');
+INSERT INTO roles (roleid, roleparam, rolename, roledisplayname, roleattribution, rolecomment) 
+VALUES (4, 'translators', 'Translator', 'Translators', 'Translation by:', 'Provided language translation.');
+INSERT INTO roles (roleid, roleparam, rolename, roledisplayname, roleattribution, rolecomment) 
+VALUES (5, 'editors', 'Editor', 'Editors', 'Edited by:', 'Provided editorial oversight.');
+
