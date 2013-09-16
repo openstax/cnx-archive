6c1f7ac21644bc41a7cf60a768e20dee8c864013
Michael Mulich <michael.mulich@gmail.com>
2013-09-16 08:05:01 -0400

    Renames main database schema file.

--- cnxarchive/database.py
+++ cnxarchive/database.py
@@ -16,7 +16,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 SQL_DIRECTORY = os.path.join(here, 'sql')
 DB_SCHEMA_DIRECTORY = os.path.join(SQL_DIRECTORY, 'schema')
 DB_SCHEMA_FILE_PATHS = (
-    os.path.join(DB_SCHEMA_DIRECTORY, 'main.sql'),
+    os.path.join(DB_SCHEMA_DIRECTORY, 'schema.sql'),
     os.path.join(DB_SCHEMA_DIRECTORY, 'trees.sql'),
     os.path.join(DB_SCHEMA_DIRECTORY, 'shred_collxml.sql'),
     os.path.join(DB_SCHEMA_DIRECTORY, 'tree_to_json.sql'),
