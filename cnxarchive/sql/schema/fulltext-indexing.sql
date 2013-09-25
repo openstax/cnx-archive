92fdb796e7c20014fcff5869a239fd513ba56ab1
Michael Mulich <michael.mulich@gmail.com>
2013-09-25 09:41:35 -0400

    Provides schema functions and triggers for fulltext indexing.

---
+++ cnxarchive/sql/schema/fulltext-indexing.sql
@@ -0,0 +1,54 @@
+CREATE EXTENSION IF NOT EXISTS plxslt;
+
+CREATE OR REPLACE FUNCTION xml_to_baretext(xml) RETURNS xml AS $$
+<?xml version="1.0"?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:cnx="http://cnx.rice.edu/cnxml"
+                xmlns:m="http://www.w3.org/1998/Math/MathML"
+                xmlns:md="http://cnx.rice.edu/mdml/0.4"
+                xmlns="http://www.w3.org/1999/xhtml"
+                xmlns:bib="http://bibtexml.sf.net/"
+                >
+
+  <xsl:output format="text" omit-xml-declaration="yes"/>
+
+  <xsl:template match="/">
+    <xsl:apply-templates/>
+  </xsl:template>
+
+  <xsl:template match="md:*"/>
+
+</xsl:stylesheet>
+$$ LANGUAGE xslt;
+
+
+CREATE OR REPLACE FUNCTION index_fulltext_trigger()
+  RETURNS TRIGGER AS $$
+  DECLARE
+    has_existing_record integer;
+    _baretext text;
+    _idx_vectors tsvector;
+  BEGIN
+    has_existing_record := (SELECT module_ident FROM modulefti WHERE module_ident = NEW.module_ident);
+    _baretext := (SELECT xml_to_baretext(convert_from(f.file, 'UTF8')::xml)::text FROM files AS f WHERE f.fileid = NEW.fileid);
+    _idx_vectors := to_tsvector(_baretext);
+
+    IF has_existing_record IS NULL THEN
+      INSERT INTO modulefti (module_ident, baretext, module_idx)
+        VALUES ( NEW.module_ident, convert_to(_baretext, 'UTF8'), _idx_vectors );
+    ELSE
+      UPDATE modulefti SET (baretext, module_idx) = ( convert_to(_baretext, 'UTF8'), _idx_vectors )
+        WHERE module_ident = NEW.module_ident;
+    END IF;
+    RETURN NEW;
+  END;
+  $$
+  LANGUAGE plpgsql;
+
+
+DROP TRIGGER IF EXISTS index_fulltext ON module_files;
+CREATE TRIGGER index_fulltext
+  AFTER INSERT OR UPDATE ON module_files
+    FOR EACH row WHEN (NEW.filename = 'index.html')
+      EXECUTE PROCEDURE index_fulltext_trigger();
