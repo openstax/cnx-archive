b6f9cd5b54d9660cb80bf4cd3e04b5ed2b72a419
Michael Mulich <michael.mulich@gmail.com>
2013-10-01 17:49:18 -0400

    Use fake views in the application tests. This makes the framework bits (application routing) less dependent on the implementation of the application.

--- cnxarchive/tests/test_application.py
+++ cnxarchive/tests/test_application.py
@@ -10,12 +10,26 @@ import unittest
 from wsgiref.util import setup_testing_defaults
 
 
+def faux_view(environ, start_response):
+    args = ', '.join(["{}={}".format(k,v)
+                      for k,v in environ['wsgiorg.routing_args']])
+    status = "200 OK"
+    headers = [('Content-type', 'text/plain',)]
+    start_response(status, headers)
+    return [args]
+
+def faux_view_one(environ, start_response):
+    return faux_view(environ, start_response)
+
+def faux_view_two(environ, start_response):
+    return faux_view(environ, start_response)
+
+
 class RoutingTest(unittest.TestCase):
 
     def test_add_route_w_fq_import(self):
         # Check that a route can be added.
-        from ..views import get_content
-        route_func = get_content
+        route_func = faux_view
         path = "/contents/{ident_hash}"
 
         from .. import Application
@@ -24,11 +38,11 @@ class RoutingTest(unittest.TestCase):
 
         environ = {'PATH_INFO': "/contents/1234abcd"}
         controller = app.route(environ)
-        self.assertEqual(controller, get_content)
+        self.assertEqual(controller, route_func)
 
     def test_add_route_w_import_str(self):
         # Check that a route can be added.
-        route_func = 'cnxarchive.views:get_content'
+        route_func = 'cnxarchive.tests.test_application:faux_view'
         path = "/contents/{ident_hash}"
 
         from .. import Application
@@ -38,7 +52,7 @@ class RoutingTest(unittest.TestCase):
         environ = {'PATH_INFO': "/contents/1234abcd"}
         controller = app.route(environ)
         from ..views import get_content
-        self.assertEqual(controller, get_content)
+        self.assertEqual(controller, faux_view)
 
     def test_route(self):
         # Check that we can route to a view, not that the route parses
@@ -46,22 +60,22 @@ class RoutingTest(unittest.TestCase):
         from .. import Application
         app = Application()
 
-        path_one = "/contents/{ident_hash}"
-        view_one = 'cnxarchive.views:get_content'
+        path_one = "/a/{b}/{c}"
+        view_one = 'cnxarchive.tests.test_application:faux_view_one'
         app.add_route(path_one, view_one)
-        path_two = "/resources/{id}"
-        view_two = 'cnxarchive.views:get_resource'
+        path_two = "/x/{y}/{z}"
+        view_two = 'cnxarchive.tests.test_application:faux_view_two'
         app.add_route(path_two, view_two)
 
-        id = '1a2b3c4d5678'
-        environ = {'PATH_INFO': '/resources/{}'.format(id)}
+        y = 'smoo'
+        z = 'oog'
+        environ = {'PATH_INFO': path_two.format(y=y, z=z)}
         setup_testing_defaults(environ)
         controller = app.route(environ)
 
-        from ..views import get_resource
-        self.assertEqual(controller, get_resource)
+        self.assertEqual(controller, faux_view_two)
         self.assertEqual(environ['wsgiorg.routing_args'],
-                         {'id': id})
+                         {'y': y, 'z': z})
 
 
 class CORSTestCase(unittest.TestCase):
