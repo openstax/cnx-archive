658a6100442f6dd8fd8f7dd1f7f79450533237b8
Ross J. Reedstrom <reedstrm@cnx.org>
2013-10-01 14:46:43 -0500

    over limiting SQL - allow for future use against any content

--- cnxarchive/sql/search/highlighted-fulltext.sql
+++ cnxarchive/sql/search/highlighted-fulltext.sql
@@ -5,11 +5,9 @@ SELECT
   ts_headline(mfti.fulltext, plainto_tsquery(%(query)s),
          'StartSel=<b>, StopSel=</b>, MinWords=600, MaxWords=700') as fulltext
 FROM
-  latest_modules as lm,
+  modules as lm,
   modulefti as mfti
 WHERE
   lm.uuid = %(id)s::uuid
-  AND
-  NOT lm.portal_type = 'Collection'
   AND
   lm.module_ident = mfti.module_ident
