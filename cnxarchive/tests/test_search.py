9ae755edcbe4c085d09e6ed234ce904a8f7ef5be
karen chan <karen@karen-chan.com>
2013-11-07 19:01:26 +0100

    Add filter only tests

--- cnxarchive/tests/test_search.py
+++ cnxarchive/tests/test_search.py
@@ -433,12 +433,53 @@ class SearchTestCase(unittest.TestCase):
         result_ids = [r['id'] for r in results]
         self.assertEqual(len(results), 0)
 
+    def test_pubYear_without_term(self):
+        self._pubYear_setup()
+        query_params = [('pubYear', '2010')]
+
+        results = self.call_target(query_params, DEFAULT_QUERY_TYPE)
+        result_ids = [r['id'] for r in results]
+        self.assertEqual(len(results), 2)
+        self.assertEqual(result_ids, ['e79ffde3-7fb4-4af3-9ec8-df648b391597',
+                                      '209deb1f-1a46-4369-9e0d-18674cf58a3e'])
+
+    def test_type_without_term(self):
+        query_params = [('type', 'book')]
+
+        results = self.call_target(query_params, DEFAULT_QUERY_TYPE)
+        result_ids = [r['id'] for r in results]
+        self.assertEqual(len(results), 1)
+        self.assertEqual(result_ids, ['e79ffde3-7fb4-4af3-9ec8-df648b391597'])
+
     def test_authorId_filter(self):
         # Filter results by author "OSC Physics Maintainer"
         query_params = [('text', 'physics'),
                         ('authorID', '1df3bab1-1dc7-4017-9b3a-960a87e706b1')]
 
         results = self.call_target(query_params)
+        result_ids = [r['id'] for r in results]
+        self.assertEqual(len(results), 1)
+        self.assertEqual(result_ids, ['209deb1f-1a46-4369-9e0d-18674cf58a3e'])
+
+    @db_connect
+    def _language_setup(self, cursor):
+        # Modify some modules to give them different languages
+        language_mods = {
+                'fr': ['209deb1f-1a46-4369-9e0d-18674cf58a3e'],
+                }
+
+        for language, ids in language_mods.iteritems():
+            cursor.execute(
+                    "UPDATE latest_modules "
+                    "SET language = %s"
+                    "WHERE uuid IN %s RETURNING module_ident",
+                    [language, tuple(ids)])
+
+    def test_language_without_term(self):
+        self._language_setup()
+        query_params = [('language', 'fr')]
+
+        results = self.call_target(query_params, DEFAULT_QUERY_TYPE)
         result_ids = [r['id'] for r in results]
         self.assertEqual(len(results), 1)
         self.assertEqual(result_ids, ['209deb1f-1a46-4369-9e0d-18674cf58a3e'])
