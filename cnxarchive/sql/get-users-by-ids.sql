b71497582a5911be9159bec83e942464c71e8d68
Michael Mulich <michael.mulich@gmail.com>
2015-02-12 17:40:27 -0500

    Replace FDW accounts with local users table.
    This removes the Foreign Data Wrapper (FDW) to the accounts database
    in favor of a local table, named users. This table will be updated
    via cnx-publishing.
    
    Some user metadata has been removed (specifically email addresses),
    because either accounts does not expose that information.

--- cnxarchive/sql/get-users-by-ids.sql
+++ cnxarchive/sql/get-users-by-ids.sql
@@ -5,13 +5,7 @@ FROM   (SELECT username                           AS id,
                first_name                         AS firstname, 
                last_name                          AS surname, 
                full_name                          AS fullname, 
-               title                              AS title, 
-               (SELECT ARRAY_AGG(value)
-                FROM   contact_infos AS ci 
-                WHERE  ci.user_id = u.id 
-                       AND type = 'EmailAddress') AS emails, 
-               NULL                               AS suffix, 
-               NULL                               AS website, 
-               NULL                               AS othername 
+               title                              AS title,
+               suffix                             AS suffix
         FROM   users AS u 
         WHERE  u.username = %s ) AS user_row
