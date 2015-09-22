5d0568545cbbaa8b6c10a5885adedefa485c75d1
Ross Reedstrom <reedstrm@gmail.com>
2015-09-22 15:50:17 -0500

    remove extra import, update copyright

--- cnxarchive/tests/test_routes.py
+++ cnxarchive/tests/test_routes.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # ###
-# Copyright (c) 2013, Rice University
+# Copyright (c) 2013, 2015 Rice University
 # This software is subject to the provisions of the GNU Affero General
 # Public License version 3 (AGPLv3).
 # See LICENCE.txt for details.
 # ###
-import os
 import unittest
 from pyramid.testing import DummyRequest
 
