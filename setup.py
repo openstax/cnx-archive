66a34d7b506e4988d2c40ddbc9f2a9924582a9ae
Ross Reedstrom <reedstrm@gmail.com>
2016-04-25 17:12:34 -0500

    version bump

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if not IS_PY3:
 
 setup(
     name='cnx-archive',
-    version='2.4.1',
+    version='2.5.0',
     author='Connexions team',
     author_email='info@cnx.org',
     url="https://github.com/connexions/cnx-archive",
