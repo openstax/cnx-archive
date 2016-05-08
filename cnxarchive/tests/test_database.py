a5ff5665b3c157fbb41fd719e382a39c064a92dc
karen chan <karen@karen-chan.com>
2016-05-08 08:29:50 +0100

    Change the db py_import test to work with source and installed paths
    Depending on whether "setup.py develop" or "setup.py install" is used,
    we get different file paths for an imported module.  We need to find
    both file paths and assert that py_import returns one of them.

--- cnxarchive/tests/test_database.py
+++ cnxarchive/tests/test_database.py
@@ -108,16 +108,44 @@ class MiscellaneousFunctionsTestCase(unittest.TestCase):
     @testing.db_connect
     def test_pyimport(self, cursor):
         target_name = 'cnxarchive.database'
-        import cnxarchive.database as target
+
+        # Import the module from current directory
+        import cnxarchive.database as target_source
+
+        # Remove current directory from sys.path
+        cwd = os.getcwd()
+        sys_path = sys.path
+        modified_path = [i for i in sys_path if i and i != cwd]
+        sys.path = modified_path
+        self.addCleanup(setattr, sys, 'path', sys_path)
+
+        # Remove all cnxarchive modules from sys.modules
+        sys_modules = sys.modules.copy()
+        self.addCleanup(sys.modules.update, sys_modules)
+        for module in sys.modules.keys():
+            if module.startswith('cnxarchive'):
+                del sys.modules[module]
+
+        # Import the installed version
+        import cnxarchive.database as target_installed
+
+        # Depending on whether "setup.py develop" or "setup.py install" is
+        # used, there are different expected directories and file paths.
+        targets = [target_source, target_installed]
+        expected_directories = [
+            os.path.abspath(os.path.dirname(target.__file__))
+            for target in targets]
+        expected_file_paths = [
+            target.__file__ for target in targets]
+
         # Check the results of calling pyimport on cnxarchive.
         cursor.execute("SELECT import, directory, file_path "
                        "FROM pyimport(%s)", (target_name,))
         import_, directory, file_path = cursor.fetchone()
 
         self.assertEqual(import_, target_name)
-        expected_directory = os.path.abspath(os.path.dirname(target.__file__))
-        self.assertEqual(directory, expected_directory)
-        self.assertEqual(file_path, target.__file__)
+        self.assertIn(directory, expected_directories)
+        self.assertIn(file_path, expected_file_paths)
 
     @testing.db_connect
     def test_pyimport_with_importerror(self, cursor):
