751368f329ad8a1cb9aff6c0560e871db1eeed63
Ross Reedstrom <reedstrm@gmail.com>
2016-09-27 11:20:47 -0500

    add another collated collection to test in-collated-book search
        Also adds tests for fulltext indexing. Update expected results for new
        collated and uncollated collection

--- cnxarchive/tests/transforms/test_resolvers.py
+++ cnxarchive/tests/transforms/test_resolvers.py
@@ -133,7 +133,7 @@ class HtmlReferenceResolutionTestCase(unittest.TestCase):
         <a href="/contents/e79ffde3-7fb4-4af3-9ec8-df648b391597:d395b566-5fe3-4428-bcb2-19016e3aa3ce#figure">
             Module link with collection
         </a>
-        <a href="/contents/e79ffde3-7fb4-4af3-9ec8-df648b391597@6.1:209deb1f-1a46-4369-9e0d-18674cf58a3e">
+        <a href="/contents/e79ffde3-7fb4-4af3-9ec8-df648b391597@6.2:209deb1f-1a46-4369-9e0d-18674cf58a3e">
             Module link with collection and version
         </a>
         <img src="/resources/d47864c2ac77d80b1f2ff4c4c7f1b2059669e3e9/Figure_01_00_01.jpg"/>
