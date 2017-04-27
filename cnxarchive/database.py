31d64036335a7239856f8d10e69ca70c9c446a12
Ross Reedstrom <reedstrm@gmail.com>
2017-04-27 13:31:44 -0500

    fixup! fixup! fix for can not publish after initial publish

--- cnxarchive/database.py
+++ cnxarchive/database.py
@@ -452,18 +452,18 @@ def assign_moduleid_default_trigger(plpy, td):
         plan = plpy.prepare("""\
 SELECT setval($1, max(substr(moduleid, $2)::int))
 FROM (
-  SELECT moduleid from modules where portal_type in $3
+  SELECT moduleid from modules where portal_type in ($3,$4)
   UNION ALL
   SELECT $4) AS all_together""", ['text', 'int', 'text', 'text'])
         args = []
         if portal_type == 'Collection':
             args.append('collectionid_seq')
             args.append(4)
-            args.append(('Collection', 'SubCollection'))
+            args.extend(('Collection', 'SubCollection'))
         elif portal_type == 'Module':
             args.append('moduleid_seq')
             args.append(2)
-            args.append(('Module', 'CompositeModule'))
+            args.extend(('Module', 'CompositeModule'))
         args.append(moduleid)
         if len(args) == 4:
             plpy.execute(plan, args)
