3a982c135a7a3589a4d7d97ee72044663b994aad
sisiz <zimmerman.sisi@gmail.com>
2017-06-02 11:17:34 -0500

    returning as json

--- cnxarchive/views/recent.py
+++ cnxarchive/views/recent.py
@@ -1,52 +1,47 @@
 import psycopg2
 from pyramid import httpexceptions
 from pyramid.view import view_config
-import calendar
+from calendar import month_name
 from pyramid.threadlocal import get_current_registry
 
 from .. import config
 
 # What should actually be the varibales in view config???
 @view_config(route_name='recent', request_method='GET',
-             accept="application/json", ## what does this mean???
-             renderer='recent.html', permission='') # originally was renderer='json',
+             accept="application/json",
+             renderer='json', permission='read') # originally was renderer='json'??,
 def get_recent(request):
+    num_entries = 10    # later can set there variables based on args in url
+    start_entry = 0
     settings = request.registry.settings
     statement = """
-                SELECT name, revised, authors, abstract, uuid, major_version, minor_version
+                SELECT name, revised, authors, abstract,
+                'http://cnx.org/contents/'||ident_hash( uuid, major_version, minor_version) AS link
                 FROM latest_modules
                 JOIN abstracts ON latest_modules.abstractid = abstracts.abstractid
+                WHERE portal_type in ('Collection', 'Module')
                 ORDER BY revised DESC
-                LIMIT 10;
-                """
+                LIMIT {};
+                """.format(num_entries)
     with psycopg2.connect(settings[config.CONNECTION_STRING]) as db_connection:
             with db_connection.cursor() as cursor:
                 cursor.execute(statement)
-                latest_modules_lists = cursor.fetchall()
+                search_results = cursor.fetchall()
 
     latest_modules = []
-    titles = ["name", "revised", "authors", "abstract", "uuid", "major_version", "minor_version"]
+    titles = ["name", "revised", "authors", "abstract", "link"]
     for i in range(len(search_results)):
         latest_modules.append(dict(zip(titles, search_results[i])))
     # set dates to be correct format
-    # get authors from persons table and format string, set the link uuid
-    # major_version, minor_version
+    # get authors from persons table
     for module in latest_modules:
         module['revised'] = format_date(module['revised'])
-        module['authors'] = format_author(module['authors'])
-        module['link'] = format_link(module['uuid'], module['major_version'],
-                                     module['minor_version'])
+        module['authors'] = format_author(module['authors'], settings)
 
     return latest_modules
 
 
-def format_link(uuid, major_version, minor_version):
-    link = "{}@{}"uuid
-    if minor_version:
-        link += "." + minor_version
-    return link
-
-def format_author(personids):
+def format_author(personids, settings):
     """
     Takes a list of personid's and searches in the persons table to get their
     full names and returns a list of the full names as a string.
@@ -74,11 +69,11 @@ def format_date(date):
     returns date as a string in the form 'May 30, 2017 at 3:03 PM'
     """
     date_string = date.isoformat()
-    day = date_string.split(" ")[0]
+    day = date_string.split("T")[0]
     day = day.split("-")
-    month = calendar.month_name[int(day[1])]
+    month = month_name[int(day[1])]
 
-    time = date_string.split(" ")[1]
+    time = date_string.split("T")[1]
     hour = int(time[:2])
     if hour < 12:
         period = "AM"
@@ -88,6 +83,5 @@ def format_date(date):
     if hour == 0:
         hour = 12
     minute = time[3:5]
-
     return "{} {}, {} at {}:{} {}".\
         format(month, day[2], day[0], hour, minute, period)
