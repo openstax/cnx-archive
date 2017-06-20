5e279435160375542852e79f0a9b5b87549cd5c2
sisiz <zimmerman.sisi@gmail.com>
2017-06-20 14:45:34 -0500

    sitemap

--- cnxarchive/views.py
+++ cnxarchive/views.py
@@ -115,17 +115,6 @@ def html_listify(tree, root_ul_element, parent_id=None):
 
 
 
-def notblocked(page):
-    """Determine if given url is a page that should be in sitemap."""
-    for blocked in PAGES_TO_BLOCK:
-        if blocked[0] != '*':
-            blocked = '*' + blocked
-        rx = compile(blocked.replace('*', '[^$]*'))
-        if rx.match(page):
-            return False
-    return True
-
-
 def _get_subject_list(cursor):
     """Return all subjects (tags) in the database except "internal" scheme."""
     subject = None
@@ -309,40 +298,4 @@ def search(request):
 
     resp.body = json.dumps(results)
 
-    return resp
-
-
-@view_config(route_name='sitemap', request_method='GET')
-def sitemap(request):
-    """Return a sitemap xml file for search engines."""
-    settings = get_current_registry().settings
-    xml = Sitemap()
-    connection_string = settings[config.CONNECTION_STRING]
-    with psycopg2.connect(connection_string) as db_connection:
-        with db_connection.cursor() as cursor:
-            # FIXME
-            # magic number limit comes from Google policy - will need to split
-            # to multiple sitemaps before we have more content
-            cursor.execute("""\
-                    SELECT
-                        ident_hash(uuid, major_version, minor_version)
-                            AS idver,
-                        REGEXP_REPLACE(TRIM(REGEXP_REPLACE(LOWER(name),
-                            '[^0-9a-z]+', ' ', 'g')), ' +', '-', 'g'),
-                        revised
-                    FROM latest_modules
-                    WHERE portal_type != 'CompositeModule'
-                    ORDER BY revised DESC LIMIT 50000""")
-            res = cursor.fetchall()
-            for ident_hash, page_name, revised in res:
-                url = request.route_url('content',
-                                        ident_hash=ident_hash,
-                                        ignore='/{}'.format(page_name))
-                if notblocked(url):
-                    xml.add_url(url, lastmod=revised)
-
-    resp = request.response
-    resp.status = '200 OK'
-    resp.content_type = 'text/xml'
-    resp.body = xml()
     return resp
