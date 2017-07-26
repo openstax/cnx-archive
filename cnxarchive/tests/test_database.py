3427302a1860956d307435f74b8080917dc60964
sisiz <zimmerman.sisi@gmail.com>
2017-07-26 14:06:08 -0500

    moved trigger code still have to move tests

--- cnxarchive/tests/test_database.py
+++ cnxarchive/tests/test_database.py
@@ -591,7 +591,7 @@ class ModulePublishTriggerTestCase(unittest.TestCase):
     def test_get_current_module_ident(self, cursor):
         cursor.execute('ALTER TABLE modules DISABLE TRIGGER module_published')
 
-        from ..database import get_current_module_ident
+        from cnxdb.triggers import get_current_module_ident
 
         cursor.execute('''INSERT INTO modules VALUES (
         DEFAULT, 'Module', 'm1', DEFAULT, '1.1', 'Name of m1',
@@ -613,7 +613,9 @@ class ModulePublishTriggerTestCase(unittest.TestCase):
 
     @testing.db_connect
     def test_next_version(self, cursor):
-        from ..database import next_version
+        cursor.execute('ALTER TABLE modules DISABLE TRIGGER module_published')
+
+        from cnxdb.triggers import next_version
 
         # Insert collection version 2.1
         cursor.execute('''INSERT INTO modules
@@ -653,7 +655,7 @@ class ModulePublishTriggerTestCase(unittest.TestCase):
     def test_get_collections(self, cursor):
         cursor.execute('ALTER TABLE modules DISABLE TRIGGER module_published')
 
-        from ..database import get_collections
+        from cnxdb.triggers import get_collections
 
         cursor.execute('''INSERT INTO modules VALUES (
         DEFAULT, 'Collection', 'col1', DEFAULT, '1.9', 'Name of c1',
@@ -702,7 +704,7 @@ class ModulePublishTriggerTestCase(unittest.TestCase):
     def test_rebuild_collection_tree(self, cursor):
         cursor.execute('ALTER TABLE modules DISABLE TRIGGER module_published')
 
-        from ..database import rebuild_collection_tree
+        from cnxdb.triggers import rebuild_collection_tree
 
         cursor.execute('''INSERT INTO modules VALUES (
         DEFAULT, 'Collection', 'col1', DEFAULT, '1.9', 'Name of c1',
@@ -776,7 +778,7 @@ class ModulePublishTriggerTestCase(unittest.TestCase):
     def test_republish_collection(self, cursor):
         cursor.execute('ALTER TABLE modules DISABLE TRIGGER module_published')
 
-        from ..database import republish_collection
+        from cnxdb.triggers import republish_collection
 
         cursor.execute("""INSERT INTO document_controls (uuid)
         VALUES ('3a5344bd-410d-4553-a951-87bccd996822'::uuid)""")
@@ -854,7 +856,7 @@ ALTER TABLE modules DISABLE TRIGGER module_published""")
         VALUES {};""".format(values_expr))
         cursor.connection.commit()
 
-        from ..database import republish_collection
+        from cnxdb.triggers import republish_collection
         new_ident = republish_collection("DEFAULT", "DEFAULT",
                                          3, collection_ident,
                                          testing.fake_plpy)
@@ -898,7 +900,7 @@ ALTER TABLE modules DISABLE TRIGGER module_published""")
 
         cursor.connection.commit()
 
-        from ..database import republish_collection
+        from cnxdb.triggers import republish_collection
         new_ident = republish_collection("DEFAULT", "DEFAULT",
                                          3, collection_ident,
                                          testing.fake_plpy)
@@ -939,7 +941,7 @@ ALTER TABLE modules DISABLE TRIGGER module_published""")
         VALUES {};""".format(values_expr))
         cursor.connection.commit()
 
-        from ..database import republish_collection
+        from cnxdb.triggers import republish_collection
         new_ident = republish_collection("DEFAULT", "DEFAULT",
                                          3, collection_ident,
                                          testing.fake_plpy)
@@ -954,7 +956,7 @@ ALTER TABLE modules DISABLE TRIGGER module_published""")
                          sorted([name for id, name in subjects]))
 
     def test_set_version(self):
-        from ..database import set_version
+        from cnxdb.triggers import set_version
 
         # set_version for modules
         td = {
@@ -994,13 +996,13 @@ ALTER TABLE modules DISABLE TRIGGER module_published""")
 
     @testing.plpy_connect
     def test_get_module_uuid(self, plpy):
-        from ..database import get_module_uuid
+        from cnxdb.triggers import get_module_uuid
         mod_uuid = get_module_uuid(plpy, 'm41237')
         self.assertEqual(mod_uuid, '91cb5f28-2b8a-4324-9373-dac1d617bc24')
 
     @testing.plpy_connect
     def test_get_subcols(self, plpy):
-        from ..database import get_subcols
+        from cnxdb.triggers import get_subcols
         subcols = tuple(get_subcols(4, plpy))
         self.assertEqual(subcols, (22,))
 
