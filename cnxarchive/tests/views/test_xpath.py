8432271a4b8c840ac597bec53ea5f24d49f62266
karen chan <karen@karen-chan.com>
2017-08-22 07:48:28 -0700

    Return the json result in html format for xpath.html
    xpath.html was returning the entire book tree whether the page contains
    the xpath or not.  xpath.json uses a completely different sql for
    getting the results.  Change them to use the same sql.

--- cnxarchive/tests/views/test_xpath.py
+++ cnxarchive/tests/views/test_xpath.py
@@ -5,8 +5,9 @@
 # Public License version 3 (AGPLv3).
 # See LICENCE.txt for details.
 # ###
-import unittest
 import HTMLParser
+import os
+import unittest
 
 try:
     from unittest import mock
@@ -78,27 +79,12 @@ class XpathViewTestCase(unittest.TestCase):
         self.request.matched_route = mock.Mock()
         self.request.matched_route.name = 'xpath'
 
-        expected = """<html xmlns="http://www.w3.org/1999/xhtml">\n  <body><ul><li><a href="/xpath.html?id=e79ffde3-7fb4-4af3-9ec8-df648b391597@7.1&amp;q=//*[local-name()='definition']">College Physics</a>\
-<ul><li><a href="/xpath.html?id=209deb1f-1a46-4369-9e0d-18674cf58a3e@7&amp;q=//*[local-name()='definition']">Preface</a>\
-</li><li><a>Introduction: The Nature of Science and Physics</a>\
-<ul><li><a href="/xpath.html?id=f3c9ab70-a916-4d8c-9256-42953287b4e9@3&amp;q=//*[local-name()='definition']">Introduction to Science and the Realm of Physics, Physical Quantities, and Units</a>\
-</li><li><a href="/xpath.html?id=d395b566-5fe3-4428-bcb2-19016e3aa3ce@4&amp;q=//*[local-name()='definition']">Physics: An Introduction</a>\
-</li><li><a href="/xpath.html?id=c8bdbabc-62b1-4a5f-b291-982ab25756d7@6&amp;q=//*[local-name()='definition']">Physical Quantities and Units</a>\
-</li><li><a href="/xpath.html?id=5152cea8-829a-4aaf-bcc5-c58a416ecb66@7&amp;q=//*[local-name()='definition']">Accuracy, Precision, and Significant Figures</a>\
-</li><li><a href="/xpath.html?id=5838b105-41cd-4c3d-a957-3ac004a48af3@5&amp;q=//*[local-name()='definition']">Approximation</a>\
-</li></ul></li><li><a>Further Applications of Newton's Laws: Friction, Drag, and Elasticity</a>\
-<ul><li><a href="/xpath.html?id=24a2ed13-22a6-47d6-97a3-c8aa8d54ac6d@2&amp;q=//*[local-name()='definition']">Introduction: Further Applications of Newton&#8217;s Laws</a>\
-</li><li><a href="/xpath.html?id=ea271306-f7f2-46ac-b2ec-1d80ff186a59@5&amp;q=//*[local-name()='definition']">Friction</a>\
-</li><li><a href="/xpath.html?id=26346a42-84b9-48ad-9f6a-62303c16ad41@6&amp;q=//*[local-name()='definition']">Drag Forces</a>\
-</li><li><a href="/xpath.html?id=56f1c5c1-4014-450d-a477-2121e276beca@8&amp;q=//*[local-name()='definition']">Elasticity: Stress and Strain</a>\
-</li></ul></li><li><a href="/xpath.html?id=f6024d8a-1868-44c7-ab65-45419ef54881@3&amp;q=//*[local-name()='definition']">Atomic Masses</a>\
-</li><li><a href="/xpath.html?id=7250386b-14a7-41a2-b8bf-9e9ab872f0dc@2&amp;q=//*[local-name()='definition']">Selected Radioactive Isotopes</a>\
-</li><li><a href="/xpath.html?id=c0a76659-c311-405f-9a99-15c71af39325@5&amp;q=//*[local-name()='definition']">Useful Inf&#248;rmation</a>\
-</li><li><a href="/xpath.html?id=ae3e18de-638d-4738-b804-dc69cd4db3a3@5&amp;q=//*[local-name()='definition']">Glossary of Key Symbols and Notation</a>\
-</li></ul></li></ul></body>\n</html>\n"""
         # Call the view
         from ...views.xpath import xpath
         resp = xpath(self.request)
+
+        with open(os.path.join(testing.here, 'data/xpath.html')) as f:
+            expected = f.read()
 
         # Check that the view returns the expected html
         self.assertMultiLineEqual(resp.body, expected)
--- cnxarchive/views/xpath.py
+++ cnxarchive/views/xpath.py
@@ -1,4 +1,6 @@
 import json
+from urllib import urlencode
+
 import psycopg2
 import psycopg2.extras
 from pyramid import httpexceptions
@@ -6,8 +8,6 @@ from lxml import etree
 from pyramid.settings import asbool
 from pyramid.threadlocal import get_current_registry, get_current_request
 from pyramid.view import view_config
-from .content import _get_content_json, HTML_WRAPPER
-from .helpers import get_content_metadata
 
 from .. import config
 from ..database import (
@@ -16,7 +16,8 @@ from ..utils import (
     IdentHashShortId, IdentHashMissingVersion, IdentHashSyntaxError,
     split_ident_hash, COLLECTION_MIMETYPE, join_ident_hash
     )
-from .helpers import get_uuid, get_latest_version
+from .content import HTML_WRAPPER
+from .helpers import get_content_metadata, get_uuid, get_latest_version
 
 
 # #################### #
@@ -28,22 +29,36 @@ def xpath_book(request, uuid, version, return_json=True):
     """
     Given a request, book UUID and version:
 
-    if return_json is False, returns the HTML tree
-    of the book, with each module title a link which returns the xpath query
-    result for that page UUID (and version if supplied, else uses most recent),
-    or
-
-    otherwise, returns a JSON object of results, each result containing:
+    returns a JSON object or HTML list of results, each result containing:
     module_name,
     module_uuid,
     xpath_results, an array of strings, each an individual xpath result.
     """
 
     xpath_string = request.params.get('q')
+    results = execute_xpath(xpath_string, 'xpath', uuid, version)
     if return_json:
-        return execute_xpath(xpath_string, 'xpath', uuid, version)
+        return results
     else:
-        return xpath_book_html(request, uuid, version)
+        return xpath_book_html(request, results)
+
+
+def xpath_book_html(request, results):
+    ul = etree.Element('ul')
+    for item in results:
+        li = etree.SubElement(ul, 'li')
+        a = etree.SubElement(li, 'a')
+        a.set('href', '{}?{}'.format(
+            request.route_path('xpath'),
+            urlencode({'q': request.params.get('q', ''),
+                       'id': item['uuid']})))
+        a.text = item['name'].decode('utf-8')
+
+        xpath_list = etree.SubElement(li, 'ul')
+        for xpath_result in item['xpath_results']:
+            li = etree.SubElement(xpath_list, 'li')
+            li.text = xpath_result.decode('utf-8')
+    return HTML_WRAPPER.format(etree.tostring(ul))
 
 
 def xpath_page(request, uuid, version):
@@ -51,76 +66,6 @@ def xpath_page(request, uuid, version):
     results, as in xpath_book()"""
     xpath_string = request.params.get('q')
     return execute_xpath(xpath_string, 'xpath-module', uuid, version)
-
-
-def _get_content_json_xpath(request, uuid, version):
-    """Return a content as a dict from its ident-hash (uuid@version)."""
-
-    settings = get_current_registry().settings
-    as_collated = asbool(request.GET.get('as_collated', True))
-    ident_hash = join_ident_hash(uuid, version)
-    with psycopg2.connect(settings[config.CONNECTION_STRING]) as db_connection:
-        with db_connection.cursor() as cursor:
-            result = get_content_metadata(uuid, version, cursor)
-            # Grab the collection tree.
-            result['tree'] = get_tree(ident_hash, cursor,
-                                      as_collated=as_collated)
-            result['collated'] = as_collated
-            if not result['tree']:
-                # If collated tree is not available, get the uncollated
-                # tree.
-                result['tree'] = get_tree(ident_hash, cursor)
-                result['collated'] = False
-
-    return result
-
-
-def xpath_book_html(request, uuid, version):
-    """Build the HTML tree."""
-    result = _get_content_json_xpath(request, uuid, version)
-
-    content = tree_to_html_xpath(result['tree'])
-
-    resp = request.response
-    resp.status = "200 OK"
-    resp.content_type = 'application/xhtml+xml'
-    resp.body = content
-    return resp
-
-
-def tree_to_html_xpath(tree):
-    """Return html list version of book tree."""
-    ul = etree.Element('ul')
-    html_listify_xpath([tree], ul)
-    return HTML_WRAPPER.format(etree.tostring(ul))
-
-
-def html_listify_xpath(tree, root_ul_element, parent_id=None):
-    """Recursively construct HTML nested list version of book tree, adding
-    hrefs to the page titles which are links to the the API endpoint which runs
-    the xpath on that page CNXML. The original caller should not call this
-    function with the `parent_id` defined.
-
-    """
-    request = get_current_request()
-    querystring = "&q=" + request.params.get('q', '')
-    is_first_node = parent_id is None
-    if is_first_node:
-        parent_id = tree[0]['id']
-    for node in tree:
-        li_elm = etree.SubElement(root_ul_element, 'li')
-        a_elm = etree.SubElement(li_elm, 'a')
-        a_elm.text = node['title']
-        if node['id'] != 'subcol':
-            if is_first_node:
-                a_elm.set('href', request.route_path('xpath') +
-                          '?id=' + node['id'] + querystring)
-            else:
-                a_elm.set('href', request.route_path('xpath') +
-                          '?id=' + node['id'] + querystring)
-        if 'contents' in node:
-            elm = etree.SubElement(li_elm, 'ul')
-            html_listify_xpath(node['contents'], elm, parent_id)
 
 
 def execute_xpath(xpath_string, sql_function, uuid, version):
@@ -192,7 +137,8 @@ def xpath(request):
             resp.body = json.dumps(results)
             resp.content_type = 'application/json'
         else:
-            return results
+            resp.body = results
+            resp.content_type = 'application/xhtml+xml'
     else:
         results = {'results': list(xpath_page(request, uuid, version))}
         resp.body = json.dumps(results)
