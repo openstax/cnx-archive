eb3b607804a044bb86d93f680a4d608dbf4cf5a3
Dante Soares <dante.m.soares@rice.edu>
2018-09-26 14:57:19 -0500

    Reduce the number of stat calls in /extras

--- cnxarchive/views/exports.py
+++ cnxarchive/views/exports.py
@@ -59,7 +59,8 @@ def get_export(request):
         with db_connection.cursor() as cursor:
             try:
                 filename, mimetype, size, modtime, state, file_content = \
-                    get_export_file(cursor, id, version, type, exports_dirs)
+                    get_export_files(cursor, id, version, [type],
+                                     exports_dirs, read_file=True)[0]
             except ExportError as e:
                 logger.debug(str(e))
                 raise httpexceptions.HTTPNotFound()
@@ -85,55 +86,83 @@ def get_export(request):
     return resp
 
 
-def get_export_file(cursor, id, version, type, exports_dirs):
-    """Retrieve file associated with document."""
+def get_export_files(cursor, id, version, types, exports_dirs, read_file=True):
+    """Retrieve files associated with document."""
     request = get_current_request()
     type_info = dict(request.registry.settings['_type_info'])
-
-    if type not in type_info:
-        raise ExportError("invalid type '{}' requested.".format(type))
-
     metadata = get_content_metadata(id, version, cursor)
-    file_extension = type_info[type]['file_extension']
-    mimetype = type_info[type]['mimetype']
-    filename = '{}@{}.{}'.format(id, version, file_extension)
     legacy_id = metadata['legacy_id']
     legacy_version = metadata['legacy_version']
-    legacy_filenames = [
-        '{}-{}.{}'.format(legacy_id, legacy_version, ext)
-        for ext in LEGACY_EXTENSION_MAP[file_extension]
-    ]
-    slugify_title_filename = u'{}-{}.{}'.format(slugify(metadata['title']),
-                                                version, file_extension)
 
-    for dir in exports_dirs:
-        if safe_stat(dir):
+    reachable_dirs = [dir for dir in exports_dirs if safe_stat(dir)]
+
+    # 1 result per type, in the same order as the given types
+    results = []
+
+    for type in list(types):
+        if type not in type_info:
+            raise ExportError("invalid type '{}' requested.".format(type))
+        file_extension = type_info[type]['file_extension']
+        mimetype = type_info[type]['mimetype']
+        filename = '{}@{}.{}'.format(id, version, file_extension)
+        legacy_filenames = [
+            '{}-{}.{}'.format(legacy_id, legacy_version, ext)
+            for ext in LEGACY_EXTENSION_MAP[file_extension]
+        ]
+        slugify_title_filename = u'{}-{}.{}'.format(slugify(metadata['title']),
+                                                    version, file_extension)
+
+        for dir in reachable_dirs:
             filepath = os.path.join(dir, filename)
             try:
-                with open(filepath, 'r') as file:
-                    stats = os.fstat(file.fileno())
-                    modtime = fromtimestamp(int(stats.st_mtime))
-                    return (slugify_title_filename, mimetype,
-                            stats.st_size, modtime, 'good', file.read())
-            except IOError:
-                # Let's see if the legacy file's there and make the new link
+                if read_file:
+                    with open(filepath, 'r') as file:
+                        stats = os.fstat(file.fileno())
+                        contents = file.read()
+                else:
+                    stats = os.stat(filepath)
+                    contents = None
+                modtime = fromtimestamp(int(stats.st_mtime))
+                results.append((slugify_title_filename, mimetype,
+                                stats.st_size, modtime, 'good', contents))
+                break
+            except EnvironmentError:
+                pass
+        else:
+            # Let's see if the legacy file's there and make the new link
+            legacy_file_found = False
+            for dir in reachable_dirs:
                 legacy_filepaths = [os.path.join(dir, fn)
                                     for fn in legacy_filenames]
                 for legacy_filepath in legacy_filepaths:
-                    if os.path.exists(legacy_filepath):
-                        with open(legacy_filepath, 'r') as file:
-                            stats = os.fstat(file.fileno())
-                            modtime = fromtimestamp(stats.st_mtime)
-                            os.link(legacy_filepath, filepath)
-                            return (slugify_title_filename, mimetype,
-                                    stats.st_size, modtime, 'good',
-                                    file.read())
-    else:
-        filenames = [filename] + legacy_filenames
-        log_formatted_filenames = '\n'.join([' - {}'.format(x)
-                                             for x in filenames])
-        logger.error("Could not find a file for '{}' at version '{}' with "
-                     "any of the following file names:\n{}"
-                     .format(id, version, log_formatted_filenames))
-        # No file, return "missing" state
-        return (slugify_title_filename, mimetype, 0, None, 'missing', None)
+                    try:
+                        if read_file:
+                            with open(legacy_filepath, 'r') as file:
+                                stats = os.fstat(file.fileno())
+                                contents = file.read()
+                        else:
+                            stats = os.stat(legacy_filepath)
+                            contents = None
+                        modtime = fromtimestamp(stats.st_mtime)
+                        os.link(legacy_filepath, filepath)
+                        results.append((slugify_title_filename, mimetype,
+                                        stats.st_size, modtime, 'good',
+                                        contents))
+                        legacy_file_found = True
+                        break
+                    except EnvironmentError:
+                        pass
+                if legacy_file_found:
+                    break
+            else:
+                filenames = [filename] + legacy_filenames
+                log_formatted_filenames = '\n'.join([' - {}'.format(x)
+                                                     for x in filenames])
+                logger.error("Could not find a file for '{}' at version '{}' "
+                             "with any of the following file names:\n{}"
+                             .format(id, version, log_formatted_filenames))
+                # No file, return "missing" state
+                results.append((slugify_title_filename, mimetype,
+                                0, None, 'missing', None))
+
+    return results
